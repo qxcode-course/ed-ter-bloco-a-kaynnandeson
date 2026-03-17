af2c0bf249d32dc71ef48e558f848e69be10b43f
Kaynnandeson Paiva <137295758+kaynnandeson@users.noreply.github.com>
2026-03-17 14:42:37 +0000

    questao da pedra

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,13 +1,61 @@
 package main
 import "fmt"
+
+type Jogador struct {
+    indice int
+    pedras [2]int
+    classificado bool
+    pontuacao int
+}
+
 func main() {
     var n int
     fmt.Scan(&n)
-    vetor := make([]int, n * 2)
+    vetorJog := make([]Jogador, n)
 
-    for i := 0; i < n * 2; i++ {
-        fmt.Scan(&vetor[i])
-        fmt.Printf("%d ", vetor[i])
+    for i := 0; i < n; i++ {
+        vetorJog[i].indice = i
+        fmt.Scan(&vetorJog[i].pedras[0])
+        fmt.Scan(&vetorJog[i].pedras[1])
+        if (vetorJog[i].pedras[0] < 10 || vetorJog[i].pedras[1] < 10) {
+            vetorJog[i].classificado = false;
+        } else  {
+            vetorJog[i].classificado = true;    
+        }
+        var temp int
+        temp = vetorJog[i].pedras[0] - vetorJog[i].pedras[1]
+        if temp < 0 {
+            vetorJog[i].pontuacao = (temp * -1)
+        } else {
+            vetorJog[i].pontuacao = temp
+        }
     }
 
+    var maiorPont, maiorInd, nDes int
+    nDes = 0
+    for i := 0; i < n; i++ {
+        if !vetorJog[i].classificado {
+            nDes++
+        }
+        if vetorJog[i].classificado {
+            maiorPont = vetorJog[i].pontuacao
+            maiorInd = vetorJog[i].indice
+        }
+    }
+
+    for i := 0; i < n; i++ {
+        if !vetorJog[i].classificado {
+            continue
+        }
+        if maiorPont > vetorJog[i].pontuacao {
+            maiorPont = vetorJog[i].pontuacao
+            maiorInd = vetorJog[i].indice
+        }
+    }
+
+    if nDes == n {
+        fmt.Printf("sem ganhador\n")
+    } else {   
+        fmt.Printf("%d\n", maiorInd)
+    }
 }
