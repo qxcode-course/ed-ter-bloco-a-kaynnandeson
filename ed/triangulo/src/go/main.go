cc563b06b2b00ed6ebe594021d27e1b20e60d17b
Kaynnandeson Paiva <137295758+kaynnandeson@users.noreply.github.com>
2026-04-08 13:57:14 +0000

    triangulo

---
+++ ed/triangulo/src/go/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func processa(vet []int) {
+	// 1. defina o ponto de parada
+	if len(vet) == 0 {
+		return
+	}
+	// 2. monte o vetor auxiliar com os resultados das somas
+	vetAux := make([]int, len(vet) - 1)
+	for i := 0; i < len(vet) - 1; i++ {
+		vetAux[i] = vet[i] + vet[i + 1]
+	}
+	// 3. chame recursivamente a função processa para o vetor auxiliar
+	processa(vetAux)
+	// 4. imprima o vetor original
+	fmt.Print("[")
+	for i := 0; i < len(vet); i++ {
+		fmt.Print(" ", vet[i])
+	}
+	fmt.Print(" ]\n")
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		return
+	}
+	line := scanner.Text()
+	parts := strings.Fields(line)
+	vet := []int{}
+	for _, part := range parts {
+		if value, err := strconv.Atoi(part); err == nil {
+			vet = append(vet, value)
+		}
+	}
+	processa(vet)
+}
+
+func Join[T any](v []T, sep string) string {
+	if len(v) == 0 {
+		return ""
+	}
+	s := ""
+	for i, x := range v {
+		if i > 0 {
+			s += sep
+		}
+		s += fmt.Sprintf("%v", x)
+	}
+	return s
+}
